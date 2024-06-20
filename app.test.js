368c92195e4a05f8f63f61a2934e95cf681a4531
jain-nabhi966 <114562478+jain-nabhi966@users.noreply.github.com>
2024-06-20 16:14:08 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,5 +1,9 @@
-const add = require('./app');
+const add = require('./mins');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
+});
+
+test('throws an error when parameters are not numbers', () => {
+    expect(() => add('1', 2)).toThrow('Both parameters must be numbers');
 });
