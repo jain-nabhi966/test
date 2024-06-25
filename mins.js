e1bbad3ab3ad75f6cafd278e6228b83befd7cba1
jain-nabhi966 <114562478+jain-nabhi966@users.noreply.github.com>
2024-06-25 17:45:19 +0530

    Update mins.js

--- mins.js
+++ mins.js
@@ -1,8 +1,9 @@
 function add(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('Both parameters must be numbers');
-  }
-  return a + b;
+  if (typeof a !== "number" || typeof b !== "number") { // Intentional error: double quotes instead of single quotes
+    throw new Error("Both parameters must be numbers") // Intentional error: missing semi-colon
+    }
+    return a + b // Intentional error: incorrect indentation
 }
 
-module.exports = add;
+module.exports = add // Intentional error: missing semi-colon
+//nabhi jain lendbox
