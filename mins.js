d26033d13e43b85034433c0787787a9ce6172dee
jain-nabhi966 <114562478+jain-nabhi966@users.noreply.github.com>
2024-06-25 16:57:10 +0530

    Update mins.js

--- mins.js
+++ mins.js
@@ -1,8 +1,8 @@
 function add(a, b) {
- if (typeof a !== "number" || typeof b !== "number") { // Intentional error: double quotes instead of single quotes
-  throw new Error("Both parameters must be numbers") // Intentional error: missing semi-colon
-  }
-  return a + b // Intentional error: incorrect indentation
+  if (typeof a !== "number" || typeof b !== "number") { // Intentional error: double quotes instead of single quotes
+    throw new Error("Both parameters must be numbers") // Intentional error: missing semi-colon
+    }
+    return a + b // Intentional error: incorrect indentation
 }
 
 module.exports = add // Intentional error: missing semi-colon
