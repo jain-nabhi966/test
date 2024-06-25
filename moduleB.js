14b45728dceea9d482183d73f0a6ae25f1a9cfb9
jain-nabhi966 <114562478+jain-nabhi966@users.noreply.github.com>
2024-06-25 19:32:30 +0530

    Create moduleB.js

---
+++ moduleB.js
@@ -0,0 +1,15 @@
+// moduleB.js
+const moduleA = require('./moduleA');
+
+console.log('Module B is loaded');
+
+module.exports = {
+  baz: () => {
+    console.log('Module B -> baz');
+  },
+  qux: () => {
+    console.log('Module B -> qux');
+  }
+};
+
+moduleA.foo(); // Calling moduleA function from moduleB
