7794024111ee8262e5d2138dfc8ed6787cb6c7d7
jain-nabhi966 <114562478+jain-nabhi966@users.noreply.github.com>
2024-06-25 13:21:44 +0530

    Update mins.js

--- mins.js
+++ mins.js
@@ -2,7 +2,7 @@ function add(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Both parameters must be numbers');
   }
-  return a + b; // Intentional error: You can change this to `return a / b;` for testing.
+  return a / b; // Intentional error: You can change this to `return a / b;` for testing.
 }
 
 module.exports = add;
