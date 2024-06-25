4fbb88ad1e5aa2cc49e9147e97dd2295f4d18c40
jain-nabhi966 <114562478+jain-nabhi966@users.noreply.github.com>
2024-06-25 17:43:11 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-        es2021: true,
-    },
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    extends: [
-        'eslint:recommended',
-    ],
-    rules: {
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-    },
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
